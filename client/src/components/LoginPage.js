2bd382c4335655443fe526c1d1d71e21deaed362
Jason Bracco <bracco.jason@gmail.com>
2022-11-20 18:33:32 -0500

    creating login page

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+function LoginPage() {
+
+    console.log("hi")
+
+
+
+    return (
+    
+        <div>
+            Hi!
+        </div>
+
+    )
+}
+
+export default LoginPage
