8ac644e3058f94c8ec7e0b1eda41efb07127aedd
Jason Bracco <bracco.jason@gmail.com>
2022-11-21 21:13:39 -0500

    setting up forms for username and password in loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,1 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
+
+function LoginForm(){
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return(
+        <div>
+            <form>
+                <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    autoComplete="off"
+                    placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="text"
+                    name="password"
+                    value={password}
+                    autoComplete="off"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </form>
+        </div>
+
+    )
+}
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import LoginForm from "./LoginForm"
+import SignupForm from "./SignupForm"
 
 function LoginPage() {
 
