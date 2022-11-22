1b0193c26054a25fe5d7b26ad45c978267ecd2f4
Jason Bracco <bracco.jason@gmail.com>
2022-11-21 21:44:44 -0500

    form setup for signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,8 +2,59 @@ import React, {useState, useEffect} from "react";
 
 function SignupForm(){
 
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [age, setAge] = useState("")
+    const [bio, setBio] = useState("")
+    const [imageURL, setImageURL] = useState("")
 
     return (
-        console.log("signup form")
+        <form>
+            <h3>Username:</h3>
+            <input
+                type="text"
+                name="username"
+                value={username}
+                autoComplete="off"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <h3>Password:</h3>
+            <input
+                type="text"
+                name="password"
+                value={password}
+                autoComplete="off"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <h3>Age:</h3>
+            <input
+                type="number"
+                name="age"
+                value={age}
+                autoComplete="off"
+                placeholder="Age"
+                onChange={(e) => setAge(e.target.value)}
+            />
+            <h3>Tell Us About Yourself:</h3>
+            <input
+                type="test"
+                name="bio"
+                value={bio}
+                autoComplete="off"
+                placeholder="Bio"
+                onChange={(e) => setBio(e.target.value)}
+            />
+            <h3>Profile Picture:</h3>
+            <input
+                type="text"
+                name="pictue"
+                value={imageURL}
+                autoComplete="off"
+                placeholder="Profile Picture"
+                onChange={(e) => setImageURL(e.target.value)}
+            />
+        </form>
     )
 }
